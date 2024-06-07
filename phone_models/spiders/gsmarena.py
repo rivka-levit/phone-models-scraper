833632b5760c4583a9348a0bea6ed7784b31796d
rivka-yuliia <juliacreadora@gmail.com>
2024-06-07 15:48:40 +0300

    parse

--- phone_models/spiders/gsmarena.py
+++ phone_models/spiders/gsmarena.py
@@ -1,12 +1,11 @@
-import scrapy
-
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
 
 from phone_models.items import PhoneModelItem
 
 
-class GsmarenaSpider(scrapy.Spider):
+class GsmarenaSpider(CrawlSpider):
     name = "gsmarena"
     allowed_domains = ["gsmarena.com"]
     start_urls = ["https://www.gsmarena.com/makers.php3"]
@@ -24,4 +23,18 @@ class GsmarenaSpider(scrapy.Spider):
     )
 
     def parse(self, response, **kwargs):
-        pass
+        item = ItemLoader(
+            PhoneModelItem(),
+            response=response,
+            selector=response
+        )
+        item.add_xpath(
+            'title',
+            './/div[@class="specs-phone-name-title"]/text()'
+        )
+        item.add_xpath(
+            'announced',
+            './/div[@id="specs-list"]/table[2]//tr[1]/td[2]/text()'
+        )
+
+        yield item.load_item()
