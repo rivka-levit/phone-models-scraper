6728f0064a4adfdb38168bad76d689d8b6969b01
rivka-yuliia <juliacreadora@gmail.com>
2024-06-07 15:35:55 +0300

    item and rules

--- phone_models/items.py
+++ phone_models/items.py
@@ -1,12 +1,10 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+"""Item model for the project."""
 
 import scrapy
 
+from itemloaders.processors import Join
 
-class PhoneModelsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+
+class PhoneModelItem(scrapy.Item):
+    title = scrapy.Field(output_processor=Join())
+    announced = scrapy.Field(output_processor=Join())
--- phone_models/spiders/gsmarena.py
+++ phone_models/spiders/gsmarena.py
@@ -1,10 +1,27 @@
 import scrapy
 
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+from phone_models.items import PhoneModelItem
+
 
 class GsmarenaSpider(scrapy.Spider):
     name = "gsmarena"
     allowed_domains = ["gsmarena.com"]
-    start_urls = ["https://gsmarena.com"]
+    start_urls = ["https://www.gsmarena.com/makers.php3"]
+    rules = (
+        Rule(
+            LinkExtractor(restrict_xpaths='//div[@class="st-text"]'),
+            callback='parse',
+            follow=True
+        ),
+        Rule(
+            LinkExtractor(restrict_xpaths='//div[@class="makers"]'),
+            callback='parse',
+            follow=True
+        )
+    )
 
     def parse(self, response, **kwargs):
         pass
